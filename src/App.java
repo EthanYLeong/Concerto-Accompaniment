cd830a38c8ae4d46bd0d9008af2dda75771f2887
Ethan Leong <ethanleong26@yahoo.com>
2026-07-11 10:49:42 -0400

    temp

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.io.File;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            Sequence sequence = MidiSystem.getSequence(new File("Tchaikovsky Violin Concerto.mid"));
+            sequencer.open();
+            sequencer.setSequence(sequence);
+            sequencer.start();
+        } catch (Exception e) {
+
+        }
     }
 }
