dd0d66546fc4d5a62c942e6b280774eca6986675
Ethan Leong <ethanleong26@yahoo.com>
2026-07-11 10:49:47 -0400

    temp

--- app/src/main/java/org/example/JavaFX.java
+++ app/src/main/java/org/example/JavaFX.java
@@ -1,12 +1,20 @@
+package org.example;
+
 import java.io.File;
 
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application {
 
     public static void main(String[] args) throws Exception {
+        launch(args);
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             Sequence sequence = MidiSystem.getSequence(new File("Tchaikovsky Violin Concerto.mid"));
@@ -16,5 +24,14 @@ public class App {
         } catch (Exception e) {
 
         }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Stage stage = new Stage();
+        HBox horizontalBox = new HBox();
+        Scene scene = new Scene(horizontalBox, 400, 400);
+        stage.setScene(scene);
+        stage.show();
     }
 }
